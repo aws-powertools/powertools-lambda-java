51eee8e9f06b2b3b3d22f0a1c48a25c7be785599
Mark Sailes <45629314+msailes@users.noreply.github.com>
2020-07-28 09:48:23 +0100

    initial working LambdaJsonAppender! (#4)

---
+++ src/main/java/software/aws/lambda/logging/PowerToolsClientException.java
@@ -0,0 +1,7 @@
+package software.aws.lambda.logging;
+
+public class PowerToolsClientException extends Error {
+    public PowerToolsClientException(String message) {
+        super(message);
+    }
+}
