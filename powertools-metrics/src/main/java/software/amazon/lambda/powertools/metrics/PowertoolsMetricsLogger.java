61c479b7ce708882cc7973465b60143ef8fc1ccf
msailes <msailes@amazon.co.uk>
2020-09-12 16:43:39 +0100

    docs: adding javadocs for the metrics core utility.

--- powertools-metrics/src/main/java/software/amazon/lambda/powertools/metrics/PowertoolsMetricsLogger.java
+++ powertools-metrics/src/main/java/software/amazon/lambda/powertools/metrics/PowertoolsMetricsLogger.java
@@ -1,17 +1,37 @@
 package software.amazon.lambda.powertools.metrics;
 
-import java.util.function.Consumer;
-
 import software.amazon.cloudwatchlogs.emf.logger.MetricsLogger;
 import software.amazon.cloudwatchlogs.emf.model.Unit;
 
+import java.util.function.Consumer;
+
+/**
+ * A class used to retrieve the instance of the {@code MetricsLogger} used by
+ * {@code PowertoolsMetrics}.
+ *
+ * {@see PowertoolsMetrics}
+ */
 public final class PowertoolsMetricsLogger {
     private static final MetricsLogger metricsLogger = new MetricsLogger();
 
+    /**
+     * The instance of the {@code MetricsLogger} used by {@code PowertoolsMetrics}.
+     *
+     * @return The instance of the MetricsLogger used by PowertoolsMetrics.
+     */
     public static MetricsLogger metricsLogger() {
         return metricsLogger;
     }
 
+    /**
+     * Add and immediately flush a single metric.
+     *
+     * @param name the name of the metric
+     * @param value the value of the metric
+     * @param unit the unit type of the metric
+     * @param namespace the namespace associated with the metric
+     * @param logger the MetricsLogger
+     */
     public static void withSingleMetric(final String name,
                                         final double value,
                                         final Unit unit,
