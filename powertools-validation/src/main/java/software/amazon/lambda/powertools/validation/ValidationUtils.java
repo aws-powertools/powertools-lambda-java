9f879ad25bdc121e2822515cc6d6788f129cde48
liuxinyu <xinyu6@illinois.edu>
2021-10-11 07:33:22 -0500

    change put() to putIfAbsent()

--- powertools-validation/src/main/java/software/amazon/lambda/powertools/validation/ValidationUtils.java
+++ powertools-validation/src/main/java/software/amazon/lambda/powertools/validation/ValidationUtils.java
@@ -229,12 +229,6 @@ public class ValidationUtils {
      * @return the loaded json schema
      */
     public static JsonSchema getJsonSchema(String schema, boolean validateSchema) {
-        JsonSchema jsonSchema = schemas.get(schema);
-
-        if (jsonSchema != null) {
-            return jsonSchema;
-        }
-
         if (schema.startsWith(CLASSPATH)) {
             String filePath = schema.substring(CLASSPATH.length());
             try (InputStream schemaStream = ValidationAspect.class.getResourceAsStream(filePath)) {
@@ -260,7 +254,7 @@ public class ValidationUtils {
             }
         }
 
-        schemas.put(schema, jsonSchema);
+        schemas.putIfAbsent(schema, jsonSchema);
 
         return jsonSchema;
     }
