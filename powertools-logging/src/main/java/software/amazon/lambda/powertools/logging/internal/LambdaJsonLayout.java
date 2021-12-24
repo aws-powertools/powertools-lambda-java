2150743a176468b5ced7cadf23181896705991e4
Pankaj Agrawal <pnkaj.agrwal@gmail.com>
2021-12-24 15:38:52 +0100

    feat: Modern way of configuring json layout via JsonTemplateLayout

--- powertools-logging/src/main/java/software/amazon/lambda/powertools/logging/internal/LambdaJsonLayout.java
+++ powertools-logging/src/main/java/software/amazon/lambda/powertools/logging/internal/LambdaJsonLayout.java
@@ -43,6 +43,9 @@ import java.util.Map;
 import static java.time.Instant.ofEpochMilli;
 import static java.time.format.DateTimeFormatter.ISO_ZONED_DATE_TIME;
 
+/***
+ *  Note: The LambdaJsonLayout should be considered to be deprecated. Please use JsonTemplateLayout instead.
+ */
 @Plugin(name = "LambdaJsonLayout", category = Node.CATEGORY, elementType = Layout.ELEMENT_TYPE, printObject = true)
 public final class LambdaJsonLayout extends AbstractJacksonLayoutCopy {
     private static final String DEFAULT_FOOTER = "]";
