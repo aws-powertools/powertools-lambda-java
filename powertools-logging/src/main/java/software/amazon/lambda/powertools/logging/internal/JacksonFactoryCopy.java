8f18865e055976595723b7fd4a9d8fe65defa88e
Pankaj Agrawal <pnkaj.agrwal@gmail.com>
2021-12-27 10:24:50 +0100

    feat: Json layout modern implementation (#670)
    * feat: Modern way of configuring json layout via JsonTemplateLayout
    
    * feat: Sample ECSLayout with powertools preconfigured
    
    * docs: Modern way of configuring json layout via JsonTemplateLayout
    
    * chore: Move modern plugins to internal package

--- powertools-logging/src/main/java/software/amazon/lambda/powertools/logging/internal/JacksonFactoryCopy.java
+++ powertools-logging/src/main/java/software/amazon/lambda/powertools/logging/internal/JacksonFactoryCopy.java
@@ -14,6 +14,7 @@ import org.apache.logging.log4j.core.jackson.Log4jJsonObjectMapper;
 import java.util.HashSet;
 import java.util.Set;
 
+@Deprecated
 abstract class JacksonFactoryCopy {
 
     static class JSON extends JacksonFactoryCopy {
---
+++ powertools-logging/src/main/java/software/amazon/lambda/powertools/logging/internal/PowertoolsResolverFactory.java
@@ -0,0 +1,34 @@
+package software.amazon.lambda.powertools.logging.internal;
+
+import org.apache.logging.log4j.core.LogEvent;
+import org.apache.logging.log4j.core.config.plugins.Plugin;
+import org.apache.logging.log4j.core.config.plugins.PluginFactory;
+import org.apache.logging.log4j.layout.template.json.resolver.EventResolverContext;
+import org.apache.logging.log4j.layout.template.json.resolver.EventResolverFactory;
+import org.apache.logging.log4j.layout.template.json.resolver.TemplateResolver;
+import org.apache.logging.log4j.layout.template.json.resolver.TemplateResolverConfig;
+import org.apache.logging.log4j.layout.template.json.resolver.TemplateResolverFactory;
+
+@Plugin(name = "PowertoolsResolverFactory", category = TemplateResolverFactory.CATEGORY)
+public final class PowertoolsResolverFactory implements EventResolverFactory {
+
+    private static final PowertoolsResolverFactory INSTANCE = new PowertoolsResolverFactory();
+
+    private PowertoolsResolverFactory() {}
+
+    @PluginFactory
+    public static PowertoolsResolverFactory getInstance() {
+        return INSTANCE;
+    }
+
+    @Override
+    public String getName() {
+        return PowertoolsResolver.getName();
+    }
+
+    @Override
+    public TemplateResolver<LogEvent> create(EventResolverContext context,
+                                             TemplateResolverConfig config) {
+        return new PowertoolsResolver();
+    }
+}
