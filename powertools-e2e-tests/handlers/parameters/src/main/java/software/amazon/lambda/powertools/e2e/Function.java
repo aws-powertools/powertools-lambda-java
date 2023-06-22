4db9822ae433b1bf63f6412ede7cc58766a334c3
Jérôme Van Der Linden <117538+jeromevdl@users.noreply.github.com>
2023-06-22 11:23:39 +0200

    fix: e2e tests on JDK8 (#1225)
    * fix e2e tests on JDK8

--- powertools-e2e-tests/handlers/parameters/src/main/java/software/amazon/lambda/powertools/e2e/Function.java
+++ powertools-e2e-tests/handlers/parameters/src/main/java/software/amazon/lambda/powertools/e2e/Function.java
@@ -2,16 +2,11 @@ package software.amazon.lambda.powertools.e2e;
 
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 import software.amazon.lambda.powertools.logging.Logging;
-import software.amazon.lambda.powertools.parameters.ParamManager;
-import software.amazon.lambda.powertools.parameters.cache.CacheManager;
 import software.amazon.lambda.powertools.parameters.AppConfigProvider;
+import software.amazon.lambda.powertools.parameters.ParamManager;
 
 public class Function implements RequestHandler<Input, String> {
-
-    private static final Logger LOG = LogManager.getLogger(Function.class);
 
     @Logging
     public String handleRequest(Input input, Context context) {
