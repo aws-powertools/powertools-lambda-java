bb9340bf8fefbc926407b27451bac0926f278ee2
Jerome Van Der Linden <jeromevdl@gmail.com>
2023-06-30 09:15:32 +0200

    refactoring and adding FIFO

--- powertools-batch/src/main/java/software/amazon/lambda/powertools/batch2/KinesisBatchProcessor.java
+++ powertools-batch/src/main/java/software/amazon/lambda/powertools/batch2/KinesisBatchProcessor.java
@@ -3,6 +3,8 @@ package software.amazon.lambda.powertools.batch2;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.events.KinesisEvent;
 import com.amazonaws.services.lambda.runtime.events.StreamsEventResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import software.amazon.lambda.powertools.utilities.EventDeserializer;
 import sun.reflect.generics.reflectiveObjects.ParameterizedTypeImpl;
 
@@ -10,19 +12,23 @@ import java.util.ArrayList;
 
 public interface KinesisBatchProcessor<ITEM> extends BatchProcessor<KinesisEvent, ITEM, StreamsEventResponse> {
 
+    Logger KINESIS_BATCH_LOGGER = LoggerFactory.getLogger(KinesisBatchProcessor.class);
+
     @Override
     default StreamsEventResponse processBatch(KinesisEvent event, Context context) {
         Class<ITEM> bodyClass = (Class<ITEM>) ((ParameterizedTypeImpl) getClass().getGenericInterfaces()[0]).getActualTypeArguments()[0];
+        boolean isKinesisRecord = bodyClass.equals(KinesisEvent.KinesisEventRecord.class);
 
         StreamsEventResponse response = StreamsEventResponse.builder().withBatchItemFailures(new ArrayList<>()).build();
         for (KinesisEvent.KinesisEventRecord record : event.getRecords()) {
             try {
-                if (bodyClass.equals(KinesisEvent.KinesisEventRecord.class)) {
+                if (isKinesisRecord) {
                     processRecord(record, context);
                 } else {
                     processItem(EventDeserializer.extractDataFrom(record).as(bodyClass), context);
                 }
             } catch (Throwable t) {
+                KINESIS_BATCH_LOGGER.error("Error while processing record with id {}: {}, adding it to batch item failures", record.getEventID(), t.getMessage());
                 response.getBatchItemFailures().add(new StreamsEventResponse.BatchItemFailure(record.getEventID()));
             }
         }
@@ -30,7 +36,7 @@ public interface KinesisBatchProcessor<ITEM> extends BatchProcessor<KinesisEvent
     }
 
     default void processRecord(KinesisEvent.KinesisEventRecord record, Context context) {
-        System.out.println("Processing message " + record.getEventID());
+        KINESIS_BATCH_LOGGER.debug("[DEFAULT IMPLEMENTATION] Processing record {}", record.getEventID());
     }
 
 }
--- powertools-batch/src/main/java/software/amazon/lambda/powertools/batch2/SQSBatchProcessor.java
+++ powertools-batch/src/main/java/software/amazon/lambda/powertools/batch2/SQSBatchProcessor.java
@@ -2,7 +2,10 @@ package software.amazon.lambda.powertools.batch2;
 
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.events.SQSBatchResponse;
+import com.amazonaws.services.lambda.runtime.events.SQSBatchResponse.BatchItemFailure;
 import com.amazonaws.services.lambda.runtime.events.SQSEvent;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import software.amazon.lambda.powertools.utilities.EventDeserializer;
 import sun.reflect.generics.reflectiveObjects.ParameterizedTypeImpl;
 
@@ -10,27 +13,59 @@ import java.util.ArrayList;
 
 public interface SQSBatchProcessor<ITEM> extends BatchProcessor<SQSEvent, ITEM, SQSBatchResponse> {
 
+    Logger SQS_BATCH_LOGGER = LoggerFactory.getLogger(SQSBatchProcessor.class);
+
+    // The attribute on an SQS-FIFO message used to record the message group ID
+    // https://docs.aws.amazon.com/lambda/latest/dg/with-sqs.html#sample-fifo-queues-message-event
+    String MESSAGE_GROUP_ID_KEY = "MessageGroupId";
+
     @Override
     default SQSBatchResponse processBatch(SQSEvent event, Context context) {
         Class<ITEM> bodyClass = (Class<ITEM>) ((ParameterizedTypeImpl) getClass().getGenericInterfaces()[0]).getActualTypeArguments()[0];
+        boolean isSQSMessage = bodyClass.getCanonicalName().equals(SQSEvent.SQSMessage.class.getCanonicalName());
 
         SQSBatchResponse response = SQSBatchResponse.builder().withBatchItemFailures(new ArrayList<>()).build();
-        for (SQSEvent.SQSMessage message : event.getRecords()) {
+
+        // If we are working on a FIFO queue, when any message fails we should stop processing and return the
+        // rest of the batch as failed too. We use this variable to track when that has happened.
+        // https://docs.aws.amazon.com/lambda/latest/dg/with-sqs.html#services-sqs-batchfailurereporting
+        boolean failWholeBatch = false;
+
+        int messageCursor = 0;
+        for (; messageCursor < event.getRecords().size() && !failWholeBatch; messageCursor++) {
+            SQSEvent.SQSMessage message = event.getRecords().get(messageCursor);
+
+            String messageGroupId = message.getAttributes() != null ?
+                    message.getAttributes().get(MESSAGE_GROUP_ID_KEY) : null;
+
             try {
-                if (bodyClass.getCanonicalName().equals(SQSEvent.SQSMessage.class.getCanonicalName())) {
+                if (isSQSMessage) {
                     processMessage(message, context);
                 } else {
                     processItem(EventDeserializer.extractDataFrom(message).as(bodyClass), context);
                 }
             } catch (Throwable t) {
-                response.getBatchItemFailures().add(new SQSBatchResponse.BatchItemFailure(message.getMessageId()));
+                SQS_BATCH_LOGGER.error("Error while processing message with messageId {}: {}, adding it to batch item failures", message.getMessageId(), t.getMessage());
+                response.getBatchItemFailures().add(BatchItemFailure.builder().withItemIdentifier(message.getMessageId()).build());
+                if (messageGroupId != null) {
+                    failWholeBatch = true;
+                    SQS_BATCH_LOGGER.info("A message in a batch with messageGroupId {} and messageId {} failed; failing the rest of the batch too"
+                            , messageGroupId, message.getMessageId());
+                }
             }
+        }
+
+        if (failWholeBatch) {
+            // Add the remaining messages to the batch item failures
+            event.getRecords()
+                    .subList(messageCursor, event.getRecords().size()) // TODO test the cursors...
+                    .forEach(message -> response.getBatchItemFailures().add(BatchItemFailure.builder().withItemIdentifier(message.getMessageId()).build()));
         }
         return response;
     }
 
     default void processMessage(SQSEvent.SQSMessage message, Context context) {
-        System.out.println("Processing message " + message.getMessageId());
+        SQS_BATCH_LOGGER.debug("[DEFAULT IMPLEMENTATION] Processing message {}", message.getMessageId());
     }
 
 }
