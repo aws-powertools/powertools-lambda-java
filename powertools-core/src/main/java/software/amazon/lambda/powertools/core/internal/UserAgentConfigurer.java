b1809ccbd7d9dea551fcbbc9226abe85a85ba585
Eleni Dimitropoulou <12170229+eldimi@users.noreply.github.com>
2023-07-21 17:31:57 +0300

    fix wrong import for logger

--- powertools-core/src/main/java/software/amazon/lambda/powertools/core/internal/UserAgentConfigurer.java
+++ powertools-core/src/main/java/software/amazon/lambda/powertools/core/internal/UserAgentConfigurer.java
@@ -1,7 +1,7 @@
 package software.amazon.lambda.powertools.core.internal;
 
-import com.sun.org.slf4j.internal.Logger;
-import com.sun.org.slf4j.internal.LoggerFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.FileInputStream;
 import java.io.IOException;
