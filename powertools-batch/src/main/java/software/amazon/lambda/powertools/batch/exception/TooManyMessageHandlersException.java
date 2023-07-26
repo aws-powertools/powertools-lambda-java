4ca1726a7803bfb8b296b6cbd5cb72ca5dad1517
Scott Gerring <gerrings@amazon.com>
2023-07-26 14:56:25 +0200

    Trying to get kinesis going

--- powertools-batch/src/main/java/software/amazon/lambda/powertools/batch/exception/TooManyMessageHandlersException.java
+++
@@ -1,9 +0,0 @@
-package software.amazon.lambda.powertools.batch.exception;
-
-public class TooManyMessageHandlersException extends RuntimeException {
-
-    public TooManyMessageHandlersException() {
-        super("You must configure either a rawMessageHandler or a messageHandler - not both");
-    }
-
-}
