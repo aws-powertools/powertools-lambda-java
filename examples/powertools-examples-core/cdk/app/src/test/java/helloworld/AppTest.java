74c9f35c0a8f1b42df2e92cfb6163ed5d5c26c05
Alexey Soshin <alexey.soshin@gmail.com>
2023-07-28 11:09:44 +0100

    Restructure CDK application info Infra and App projects

--- examples/powertools-examples-core/cdk/app/src/test/java/helloworld/AppTest.java
+++ examples/powertools-examples-core/cdk/app/src/test/java/helloworld/AppTest.java
@@ -14,26 +14,26 @@
 
 package helloworld;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
 
 import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
 import com.amazonaws.xray.AWSXRay;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
 
 public class AppTest {
 
-    @BeforeEach
+    @Before
     public void setup() {
         if (null == System.getenv("LAMBDA_TASK_ROOT")) {
             AWSXRay.beginSegment("test");
         }
     }
 
-    @AfterEach
+    @After
     public void tearDown() {
         if (AWSXRay.getCurrentSubsegmentOptional().isPresent()) {
             AWSXRay.endSubsegment();
@@ -48,8 +48,8 @@ public class AppTest {
     public void successfulResponse() {
         App app = new App();
         APIGatewayProxyResponseEvent result = app.handleRequest(null, null);
-        assertEquals(200, result.getStatusCode());
-        assertEquals("application/json", result.getHeaders().get("Content-Type"));
+        assertEquals(result.getStatusCode().intValue(), 200);
+        assertEquals(result.getHeaders().get("Content-Type"), "application/json");
         String content = result.getBody();
         assertNotNull(content);
         assertTrue(content.contains("\"message\""));
---
+++ examples/powertools-examples-core/cdk/infra/src/main/java/cdk/CdkStack.java
@@ -0,0 +1,44 @@
+package cdk;
+
+
+import software.amazon.awscdk.services.lambda.Code;
+import software.amazon.awscdk.services.lambda.Function;
+import software.constructs.Construct;
+import software.amazon.awscdk.Stack;
+import software.amazon.awscdk.StackProps;
+// import software.amazon.awscdk.Duration;
+// import software.amazon.awscdk.services.sqs.Queue;
+import software.amazon.awscdk.App;
+import software.amazon.awscdk.BundlingOptions;
+import software.amazon.awscdk.CfnOutput;
+import software.amazon.awscdk.CfnOutputProps;
+import software.constructs.Construct;
+import software.amazon.awscdk.DockerVolume;
+import software.amazon.awscdk.Duration;
+import software.amazon.awscdk.Stack;
+import software.amazon.awscdk.StackProps;
+import software.amazon.awscdk.services.lambda.Code;
+import software.amazon.awscdk.services.lambda.Function;
+import software.amazon.awscdk.services.lambda.FunctionProps;
+import software.amazon.awscdk.services.lambda.Runtime;
+import software.amazon.awscdk.services.logs.RetentionDays;
+import software.amazon.awscdk.services.s3.assets.AssetOptions;
+
+public class CdkStack extends Stack {
+    public CdkStack(final Construct scope, final String id) {
+        this(scope, id, null);
+    }
+
+    public CdkStack(final Construct scope, final String id, final StackProps props) {
+        super(scope, id, props);
+
+        // The code that defines your stack goes here
+
+        final Function hello = Function.Builder.create(this, "HelloWorldFunction")
+                .runtime(Runtime.JAVA_11)    // execution environment
+                .code(Code.fromAsset("../helloworld/", AssetOptions.builder()
+                        .build()))  // code loaded from the "lambda" directory
+                .handler("helloworld.App::handleRequest")        // file is "hello", function is "handler"
+                .build();
+    }
+}
