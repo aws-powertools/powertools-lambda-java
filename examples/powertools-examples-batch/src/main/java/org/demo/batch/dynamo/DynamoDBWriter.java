00fe0b05e200431c10c37139a79e188284c4a85c
Michele Ricciardi <mriccia@amazon.com>
2023-08-02 15:02:12 +0200

    add items updates and deletions to ddb example

--- examples/powertools-examples-batch/src/main/java/org/demo/batch/dynamo/DynamoDBWriter.java
+++ examples/powertools-examples-batch/src/main/java/org/demo/batch/dynamo/DynamoDBWriter.java
@@ -4,7 +4,9 @@ import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.amazonaws.services.lambda.runtime.events.ScheduledEvent;
 import java.security.SecureRandom;
+import java.util.List;
 import java.util.UUID;
+import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -42,27 +44,65 @@ public class DynamoDBWriter implements RequestHandler<ScheduledEvent, String> {
 
         LOGGER.info("handleRequest");
 
+        List<DdbProduct> products = createProducts(tableName);
+        List<DdbProduct> updatedProducts = updateProducts(tableName, products);
+        deleteProducts(tableName, updatedProducts);
+
+        return "Success";
+    }
+
+    private void deleteProducts(String tableName, List<DdbProduct> updatedProducts) {
+        WriteBatch.Builder<DdbProduct> productDeleteBuilder = WriteBatch.builder(DdbProduct.class)
+                .mappedTableResource(enhancedClient.table(tableName, TableSchema.fromBean(DdbProduct.class)));
+
+        updatedProducts.forEach(productDeleteBuilder::addDeleteItem);
+
+        BatchWriteResult batchDeleteResult = enhancedClient
+                .batchWriteItem(BatchWriteItemEnhancedRequest.builder().writeBatches(
+                        productDeleteBuilder.build())
+                .build());
+        LOGGER.info("Deleted batch of objects from DynamoDB: {}", batchDeleteResult);
+    }
+
+    private List<DdbProduct> updateProducts(String tableName, List<DdbProduct> products) {
+        WriteBatch.Builder<DdbProduct> productUpdateBuilder = WriteBatch.builder(DdbProduct.class)
+                .mappedTableResource(enhancedClient.table(tableName, TableSchema.fromBean(DdbProduct.class)));
+
+        List<DdbProduct> updatedProducts = products.stream().map(product -> {
+            // Update the price of the product and add it to the batch
+            LOGGER.info("Updating product: {}", product);
+            float price = random.nextFloat();
+            DdbProduct updatedProduct = new DdbProduct(product.getId(), "updated-product-" + product.getId(), price);
+            productUpdateBuilder.addPutItem(updatedProduct);
+            return updatedProduct;
+        }).collect(Collectors.toList());
+
+        BatchWriteResult batchUpdateResult = enhancedClient
+                .batchWriteItem(BatchWriteItemEnhancedRequest.builder().writeBatches(
+                        productUpdateBuilder.build())
+                .build());
+        LOGGER.info("Updated batch of objects to DynamoDB: {}", batchUpdateResult);
+        return updatedProducts;
+    }
+
+    public List<DdbProduct> createProducts(String tableName) {
         WriteBatch.Builder<DdbProduct> productBuilder = WriteBatch.builder(DdbProduct.class)
                 .mappedTableResource(enhancedClient.table(tableName, TableSchema.fromBean(DdbProduct.class)));
 
-        IntStream.range(0, 5).forEach(i -> {
+        List<DdbProduct> ddbProductStream = IntStream.range(0, 5).mapToObj(i -> {
             String id = UUID.randomUUID().toString();
-
             float price = random.nextFloat();
-            DdbProduct product = new DdbProduct(id, "product-" + id, price);
+            // Create a new product and add it to the batch
+            final DdbProduct product = new DdbProduct(id, "product-" + id, price);
             productBuilder.addPutItem(product);
-        });
+            return product;
+        }).collect(Collectors.toList());
 
-        BatchWriteItemEnhancedRequest batchWriteItemEnhancedRequest =
-                BatchWriteItemEnhancedRequest.builder().writeBatches(
+        BatchWriteResult batchWriteResult = enhancedClient
+                .batchWriteItem(BatchWriteItemEnhancedRequest.builder().writeBatches(
                                 productBuilder.build())
-                        .build();
-
-        BatchWriteResult batchWriteResult = enhancedClient.batchWriteItem(batchWriteItemEnhancedRequest);
-
-
-        LOGGER.info("Wrote batch of messages to DynamoDB: {}", batchWriteResult);
-
-        return "Success";
+                        .build());
+        LOGGER.info("Wrote batch of objects to DynamoDB: {}", batchWriteResult);
+        return ddbProductStream;
     }
 }
