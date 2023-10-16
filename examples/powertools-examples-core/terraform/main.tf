86155a9bdac8469f6383326c8416c26c1b883525
Pascal Romanens <promanen@amazon.ch>
2023-10-16 13:40:13 +0200

    #1458 add LAMBDA_TASK_ROOT at pom level, removed tf plan for now, changed tf providers to pass tf validate

--- examples/powertools-examples-core/terraform/main.tf
+++ examples/powertools-examples-core/terraform/main.tf
@@ -1,3 +1,12 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
 # terraform modules
 module "powertools_for_java_lambda" {
   source = "./infra/"
@@ -6,4 +15,9 @@ module "powertools_for_java_lambda" {
 output "api_url" {
   value       = module.powertools_for_java_lambda.invoke
   description = "URL where the API gateway can be invoked"
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "us-east-1"
 }
