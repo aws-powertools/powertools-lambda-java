6b8fe49dddf33c8d63b1346a365b3e7cfdff4f55
Jérôme Van Der Linden <117538+jeromevdl@users.noreply.github.com>
2023-11-16 13:23:18 +0100

    feat: ALC (#1514)
    * handle AWS_LAMBDA_LOG configuration
    
    * ALC documentation + code review
    
    * update doc

---
+++ powertools-logging/src/main/java/software/amazon/lambda/powertools/logging/internal/LambdaTimestampResolverFactory.java
@@ -0,0 +1,49 @@
+/*
+ * Copyright 2023 Amazon.com, Inc. or its affiliates.
+ * Licensed under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package software.amazon.lambda.powertools.logging.internal;
+
+import org.apache.logging.log4j.core.LogEvent;
+import org.apache.logging.log4j.core.config.plugins.Plugin;
+import org.apache.logging.log4j.core.config.plugins.PluginFactory;
+import org.apache.logging.log4j.layout.template.json.resolver.EventResolverContext;
+import org.apache.logging.log4j.layout.template.json.resolver.EventResolverFactory;
+import org.apache.logging.log4j.layout.template.json.resolver.TemplateResolver;
+import org.apache.logging.log4j.layout.template.json.resolver.TemplateResolverConfig;
+import org.apache.logging.log4j.layout.template.json.resolver.TemplateResolverFactory;
+
+@Plugin(name = "LambdaTimestampResolverFactory", category = TemplateResolverFactory.CATEGORY)
+public final class LambdaTimestampResolverFactory implements EventResolverFactory {
+
+    private static final LambdaTimestampResolverFactory INSTANCE = new LambdaTimestampResolverFactory();
+
+    private LambdaTimestampResolverFactory() {
+    }
+
+    @PluginFactory
+    public static LambdaTimestampResolverFactory getInstance() {
+        return INSTANCE;
+    }
+
+    @Override
+    public String getName() {
+        return LambdaTimestampResolver.getName();
+    }
+
+    @Override
+    public TemplateResolver<LogEvent> create(EventResolverContext context,
+                                             TemplateResolverConfig config) {
+        return new LambdaTimestampResolver(config);
+    }
+}
