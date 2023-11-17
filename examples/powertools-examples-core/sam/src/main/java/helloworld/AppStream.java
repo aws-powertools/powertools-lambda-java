e932d109d33d6be16b7961d6d24749294874ee06
Jason Harris <harrzjas@amazon.co.uk>
2023-11-17 16:48:33 +0000

    Refactoring stream function to process input logging example with a Lambda Function URL instead of APIGW.

--- examples/powertools-examples-core/sam/src/main/java/helloworld/AppStream.java
+++ examples/powertools-examples-core/sam/src/main/java/helloworld/AppStream.java
@@ -17,22 +17,47 @@ package helloworld;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestStreamHandler;
 import com.fasterxml.jackson.databind.ObjectMapper;
+
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.util.Map;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import software.amazon.lambda.powertools.logging.Logging;
 import software.amazon.lambda.powertools.metrics.Metrics;
 
+import java.io.InputStreamReader;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.nio.charset.Charset;
+
 public class AppStream implements RequestStreamHandler {
     private static final ObjectMapper mapper = new ObjectMapper();
+    private final static Logger log = LogManager.getLogger(AppStream.class);
 
     @Override
     @Logging(logEvent = true)
     @Metrics(namespace = "ServerlessAirline", service = "payment", captureColdStart = true)
     public void handleRequest(InputStream input, OutputStream output, Context context) throws IOException {
-        Map map = mapper.readValue(input, Map.class);
-
-        System.out.println(map.size());
+        BufferedReader reader = new BufferedReader(new InputStreamReader(input, Charset.forName("US-ASCII")));
+        PrintWriter writer = new PrintWriter(new BufferedWriter(new OutputStreamWriter(output, Charset.forName("US-ASCII"))));
+        int nextChar;
+        try {
+            while ((nextChar = reader.read()) != -1) {
+                output.write(nextChar);
+            }
+        } catch (IOException e) {
+            log.error("Something has gone wrong: ", e);
+        } finally {
+            reader.close();
+            String finalString = writer.toString();
+            log.info("Final string result: " + finalString);
+            writer.close();
+        }
     }
 }
+
