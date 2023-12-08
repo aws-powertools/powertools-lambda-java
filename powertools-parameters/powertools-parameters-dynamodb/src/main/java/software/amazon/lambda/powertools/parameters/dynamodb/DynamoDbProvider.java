fab773e04c308ca1e8406f00e196c989f7e6d36a
Scott Gerring <gerrings@amazon.com>
2023-12-08 15:25:02 +0100

    Removed useless create

--- powertools-parameters/powertools-parameters-dynamodb/src/main/java/software/amazon/lambda/powertools/parameters/dynamodb/DynamoDbProvider.java
+++ powertools-parameters/powertools-parameters-dynamodb/src/main/java/software/amazon/lambda/powertools/parameters/dynamodb/DynamoDbProvider.java
@@ -57,13 +57,6 @@ public class DynamoDbProvider extends BaseProvider {
     }
 
     /**
-     * Create a DynamoDbProvider with all default settings.
-     */
-    public static DynamoDbProvider create() {
-        return new DynamoDbProviderBuilder().build();
-    }
-
-    /**
      * Return a single value from the DynamoDB parameter provider.
      *
      * @param key key of the parameter
