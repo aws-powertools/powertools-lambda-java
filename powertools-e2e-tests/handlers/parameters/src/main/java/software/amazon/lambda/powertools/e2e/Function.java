487bd77bf5efa75af271c0611903f0af55d3e11c
Scott Gerring <gerrings@amazon.com>
2023-12-08 16:03:01 +0100

    Clean up E2E test tree

--- powertools-e2e-tests/handlers/parameters/src/main/java/software/amazon/lambda/powertools/e2e/Function.java
+++ powertools-e2e-tests/handlers/parameters/src/main/java/software/amazon/lambda/powertools/e2e/Function.java
@@ -17,8 +17,7 @@ package software.amazon.lambda.powertools.e2e;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import software.amazon.lambda.powertools.logging.Logging;
-import software.amazon.lambda.powertools.parameters.AppConfigProvider;
-import software.amazon.lambda.powertools.parameters.ParamManager;
+import software.amazon.lambda.powertools.parameters.appconfig.AppConfigProvider;
 
 public class Function implements RequestHandler<Input, String> {
 
