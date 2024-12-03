1a2cad2cbc7c2150b9331f6e8ea702481f51d823
Rakshith Rao <rakrao@amazon.de>
2024-12-03 11:16:17 +0100

    Added GRM for powertools-metrics and update the example

--- examples/powertools-examples-core-utilities/sam-graalvm/src/main/java/helloworld/App.java
+++ examples/powertools-examples-core-utilities/sam-graalvm/src/main/java/helloworld/App.java
@@ -15,6 +15,9 @@
 package helloworld;
 
 import static software.amazon.lambda.powertools.logging.argument.StructuredArguments.entry;
+import static software.amazon.lambda.powertools.metrics.MetricsUtils.metricsLogger;
+import static software.amazon.lambda.powertools.metrics.MetricsUtils.withSingleMetric;
+import static software.amazon.lambda.powertools.tracing.TracingUtils.putMetadata;
 
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
@@ -27,36 +30,64 @@ import java.net.URL;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.stream.Collectors;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.slf4j.MDC;
+import software.amazon.cloudwatchlogs.emf.model.DimensionSet;
+import software.amazon.cloudwatchlogs.emf.model.StorageResolution;
+import software.amazon.cloudwatchlogs.emf.model.Unit;
 import software.amazon.lambda.powertools.logging.Logging;
-
+import software.amazon.lambda.powertools.metrics.Metrics;
+import software.amazon.lambda.powertools.tracing.CaptureMode;
+import software.amazon.lambda.powertools.tracing.Tracing;
+import software.amazon.lambda.powertools.tracing.TracingUtils;
 
 /**
  * Handler for requests to Lambda function.
  */
 public class App implements RequestHandler<APIGatewayProxyRequestEvent, APIGatewayProxyResponseEvent> {
-    private final static Logger log = LogManager.getLogger(App.class);
+    private static final Logger log = LoggerFactory.getLogger(App.class);
 
     @Logging(logEvent = true, samplingRate = 0.7)
+    @Tracing(captureMode = CaptureMode.RESPONSE_AND_ERROR)
+    @Metrics(namespace = "ServerlessAirline", service = "payment", captureColdStart = true)
     public APIGatewayProxyResponseEvent handleRequest(final APIGatewayProxyRequestEvent input, final Context context) {
         Map<String, String> headers = new HashMap<>();
 
         headers.put("Content-Type", "application/json");
         headers.put("X-Custom-Header", "application/json");
 
+        metricsLogger().putMetric("CustomMetric1", 1, Unit.COUNT);
+
+        withSingleMetric("CustomMetrics2", 1, Unit.COUNT, "Another", (metric) ->
+        {
+            metric.setDimensions(DimensionSet.of("AnotherService", "CustomService"));
+            metric.setDimensions(DimensionSet.of("AnotherService1", "CustomService1"));
+        });
+
+        metricsLogger().putMetric("CustomMetric3", 1, Unit.COUNT, StorageResolution.HIGH);
+
+        MDC.put("test", "willBeLogged");
+
         APIGatewayProxyResponseEvent response = new APIGatewayProxyResponseEvent()
                 .withHeaders(headers);
         try {
-            log.info("Initialisation type: {}", System.getenv("AWS_LAMBDA_INITIALIZATION_TYPE"));
             final String pageContents = this.getPageContents("https://checkip.amazonaws.com");
             log.info("", entry("ip", pageContents));
+            TracingUtils.putAnnotation("Test", "New");
             String output = String.format("{ \"message\": \"hello world\", \"location\": \"%s\" }", pageContents);
 
-            log.info("After output");
+            TracingUtils.withSubsegment("loggingResponse", subsegment ->
+            {
+                String sampled = "log something out";
+                log.info(sampled);
+                log.info(output);
+            });
 
-            //Return a response with body
-            return new APIGatewayProxyResponseEvent().withHeaders(headers).withStatusCode(200).withBody(output);
+            log.info("After output");
+            return response
+                    .withStatusCode(200)
+                    .withBody(output);
         } catch (RuntimeException | IOException e) {
             return response
                     .withBody("{}")
@@ -64,8 +95,15 @@ public class App implements RequestHandler<APIGatewayProxyRequestEvent, APIGatew
         }
     }
 
+    @Tracing
+    private void log() {
+        log.info("inside threaded logging for function");
+    }
+
+    @Tracing(namespace = "getPageContents", captureMode = CaptureMode.DISABLED)
     private String getPageContents(String address) throws IOException {
         URL url = new URL(address);
+        putMetadata("getPageContents", address);
         try (BufferedReader br = new BufferedReader(new InputStreamReader(url.openStream()))) {
             return br.lines().collect(Collectors.joining(System.lineSeparator()));
         }
