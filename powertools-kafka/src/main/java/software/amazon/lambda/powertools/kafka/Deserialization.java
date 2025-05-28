4282a77223a4a9c82b5e36c77cebde1f2ba64d7d
Philipp Page <pagejep@amazon.com>
2025-05-28 09:44:48 +0200

    Add missing copyright.

--- powertools-kafka/src/main/java/software/amazon/lambda/powertools/kafka/Deserialization.java
+++ powertools-kafka/src/main/java/software/amazon/lambda/powertools/kafka/Deserialization.java
@@ -10,7 +10,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package software.amazon.lambda.powertools.kafka;
 
 import java.lang.annotation.ElementType;
