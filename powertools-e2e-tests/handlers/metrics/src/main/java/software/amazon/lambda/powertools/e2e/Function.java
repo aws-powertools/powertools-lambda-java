120b8d0c958fe29e2b0e97fac5d63409fdb339fa
Philipp Page <pagejep@amazon.com>
2025-06-05 16:06:07 +0200

    Fix namespace regex and fix metrics e2e tests.

--- powertools-e2e-tests/handlers/metrics/src/main/java/software/amazon/lambda/powertools/e2e/Function.java
+++ powertools-e2e-tests/handlers/metrics/src/main/java/software/amazon/lambda/powertools/e2e/Function.java
@@ -16,36 +16,35 @@ package software.amazon.lambda.powertools.e2e;
 
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
-import software.amazon.cloudwatchlogs.emf.logger.MetricsLogger;
-import software.amazon.cloudwatchlogs.emf.model.DimensionSet;
-import software.amazon.cloudwatchlogs.emf.model.StorageResolution;
-import software.amazon.cloudwatchlogs.emf.model.Unit;
 import software.amazon.lambda.powertools.metrics.FlushMetrics;
-import software.amazon.lambda.powertools.metrics.MetricsUtils;
+import software.amazon.lambda.powertools.metrics.Metrics;
+import software.amazon.lambda.powertools.metrics.MetricsFactory;
+import software.amazon.lambda.powertools.metrics.model.DimensionSet;
+import software.amazon.lambda.powertools.metrics.model.MetricResolution;
+import software.amazon.lambda.powertools.metrics.model.MetricUnit;
 import java.time.LocalDateTime;
 import java.time.ZoneOffset;
 import java.time.temporal.ChronoUnit;
 import java.time.Instant;
 
-
 public class Function implements RequestHandler<Input, String> {
 
-    MetricsLogger metricsLogger = MetricsUtils.metricsLogger();
+    Metrics metrics = MetricsFactory.getMetricsInstance();
 
     @FlushMetrics(captureColdStart = true)
     public String handleRequest(Input input, Context context) {
 
-        Instant currentTimeTruncatedPlusThirty =
-                LocalDateTime.now().truncatedTo(ChronoUnit.MINUTES).toInstant(ZoneOffset.UTC).plusSeconds(30);
-        metricsLogger.setTimestamp(currentTimeTruncatedPlusThirty);
+        Instant currentTimeTruncatedPlusThirty = LocalDateTime.now().truncatedTo(ChronoUnit.MINUTES)
+                .toInstant(ZoneOffset.UTC).plusSeconds(30);
+        metrics.setTimestamp(currentTimeTruncatedPlusThirty);
 
         DimensionSet dimensionSet = new DimensionSet();
         input.getDimensions().forEach((key, value) -> dimensionSet.addDimension(key, value));
-        metricsLogger.putDimensions(dimensionSet);
+        metrics.addDimension(dimensionSet);
 
-        input.getMetrics().forEach((key, value) -> metricsLogger.putMetric(key, value, Unit.COUNT,
-                input.getHighResolution().equalsIgnoreCase("true") ? StorageResolution.HIGH :
-                        StorageResolution.STANDARD));
+        input.getMetrics().forEach((key, value) -> metrics.addMetric(key, value, MetricUnit.COUNT,
+                input.getHighResolution().equalsIgnoreCase("true") ? MetricResolution.HIGH
+                        : MetricResolution.STANDARD));
 
         return "OK";
     }
