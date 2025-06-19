5610673795992bb564e5ceb4ddc92869df379947
Jeroen Reijn <j.reijn@gmail.com>
2025-06-19 18:24:13 +0200

    feat(serialization): Split SAM and SAM native example

---
+++ examples/powertools-examples-serialization/sam/src/main/java/org/demo/serialization/APIGatewayRequestDeserializationFunction.java
@@ -0,0 +1,53 @@
+/*
+ * Copyright 2023 Amazon.com, Inc. or its affiliates.
+ * Licensed under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package org.demo.serialization;
+
+import static software.amazon.lambda.powertools.utilities.EventDeserializer.extractDataFrom;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
+import java.util.HashMap;
+import java.util.Map;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
+public class APIGatewayRequestDeserializationFunction
+        implements RequestHandler<APIGatewayProxyRequestEvent, APIGatewayProxyResponseEvent> {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(APIGatewayRequestDeserializationFunction.class);
+    private static final Map<String, String> HEADERS = new HashMap<String, String>() {
+        private static final long serialVersionUID = 7074189990115081999L;
+        {
+        put("Content-Type", "application/json");
+        put("X-Custom-Header", "application/json");
+        }
+    };
+
+    public APIGatewayProxyResponseEvent handleRequest(APIGatewayProxyRequestEvent event, Context context) {
+
+        Product product = extractDataFrom(event).as(Product.class);
+        LOGGER.info("\n=============== Deserialized request body: ===============");
+        LOGGER.info("product={}\n", product);
+
+        return new APIGatewayProxyResponseEvent()
+                .withHeaders(HEADERS)
+                .withStatusCode(200)
+                .withBody("Received request for productId: " + product.getId());
+    }
+}
+
