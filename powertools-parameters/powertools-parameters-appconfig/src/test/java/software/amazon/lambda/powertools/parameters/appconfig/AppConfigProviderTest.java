2bb51d8064919ac4795c8ab8b6fb603fc2c65530
Philipp Page <github@philipp.page>
2025-07-29 09:59:19 +0200

    fix(parameters): Correctly check for empty values in AppConfig Parameters Provider. (#1982)
    * fix(parameters): Correctly check for empty values in AppConfig Parameters Provider.
    
    * fix pmd findings.

--- powertools-parameters/powertools-parameters-appconfig/src/test/java/software/amazon/lambda/powertools/parameters/appconfig/AppConfigProviderTest.java
+++ powertools-parameters/powertools-parameters-appconfig/src/test/java/software/amazon/lambda/powertools/parameters/appconfig/AppConfigProviderTest.java
@@ -14,9 +14,9 @@
 
 package software.amazon.lambda.powertools.parameters.appconfig;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatIllegalStateException;
 import static org.assertj.core.api.Assertions.assertThatRuntimeException;
-import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.mockito.MockitoAnnotations.openMocks;
 import static software.amazon.lambda.powertools.parameters.transform.Transformer.json;
@@ -27,6 +27,7 @@ import org.mockito.ArgumentCaptor;
 import org.mockito.Captor;
 import org.mockito.Mock;
 import org.mockito.Mockito;
+
 import software.amazon.awssdk.core.SdkBytes;
 import software.amazon.awssdk.services.appconfigdata.AppConfigDataClient;
 import software.amazon.awssdk.services.appconfigdata.model.GetLatestConfigurationRequest;
@@ -36,11 +37,11 @@ import software.amazon.awssdk.services.appconfigdata.model.StartConfigurationSes
 import software.amazon.lambda.powertools.parameters.cache.CacheManager;
 import software.amazon.lambda.powertools.parameters.transform.TransformationManager;
 
-public class AppConfigProviderTest {
+class AppConfigProviderTest {
 
-    private final String environmentName = "test";
-    private final String applicationName = "fakeApp";
-    private final String defaultTestKey = "key1";
+    private static final String ENVIRONMENT_NAME = "test";
+    private static final String DEFAULT_TEST_KEY = "key1";
+    private static final String APPLICATION_NAME = "fakeApp";
 
     @Mock
     AppConfigDataClient client;
@@ -53,18 +54,17 @@ public class AppConfigProviderTest {
     private AppConfigProvider provider;
 
     @BeforeEach
-    public void init() {
+    void init() {
         openMocks(this);
 
         provider = AppConfigProvider.builder()
                 .withClient(client)
-                .withApplication(applicationName)
-                .withEnvironment(environmentName)
+                .withApplication(APPLICATION_NAME)
+                .withEnvironment(ENVIRONMENT_NAME)
                 .withCacheManager(new CacheManager())
                 .withTransformationManager(new TransformationManager())
                 .build();
     }
-
 
     /**
      * Tests repeated calls to the AppConfigProvider for the same key behave correctly. This is more complicated than
@@ -73,7 +73,7 @@ public class AppConfigProviderTest {
      * subsequent calls should once again return the new data.
      */
     @Test
-    public void getValueRetrievesValue() {
+    void getValueRetrievesValue() {
         // Arrange
         StartConfigurationSessionResponse firstSession = StartConfigurationSessionResponse.builder()
                 .initialConfigurationToken("token1")
@@ -92,24 +92,32 @@ public class AppConfigProviderTest {
         GetLatestConfigurationResponse thirdResponse = GetLatestConfigurationResponse.builder()
                 .nextPollConfigurationToken("token4")
                 .build();
+        // Forth response returns empty, which means the provider should yield the previous value again
+        GetLatestConfigurationResponse forthResponse = GetLatestConfigurationResponse.builder()
+                .nextPollConfigurationToken("token5")
+                .configuration(SdkBytes.fromUtf8String(""))
+                .build();
         Mockito.when(client.startConfigurationSession(startSessionRequestCaptor.capture()))
                 .thenReturn(firstSession);
         Mockito.when(client.getLatestConfiguration(getLatestConfigurationRequestCaptor.capture()))
-                .thenReturn(firstResponse, secondResponse, thirdResponse);
+                .thenReturn(firstResponse, secondResponse, thirdResponse, forthResponse);
 
         // Act
-        String returnedValue1 = provider.getValue(defaultTestKey);
-        String returnedValue2 = provider.getValue(defaultTestKey);
-        String returnedValue3 = provider.getValue(defaultTestKey);
+        String returnedValue1 = provider.getValue(DEFAULT_TEST_KEY);
+        String returnedValue2 = provider.getValue(DEFAULT_TEST_KEY);
+        String returnedValue3 = provider.getValue(DEFAULT_TEST_KEY);
+        String returnedValue4 = provider.getValue(DEFAULT_TEST_KEY);
 
         // Assert
         assertThat(returnedValue1).isEqualTo(firstResponse.configuration().asUtf8String());
         assertThat(returnedValue2).isEqualTo(secondResponse.configuration().asUtf8String());
         assertThat(returnedValue3).isEqualTo(secondResponse.configuration()
                 .asUtf8String()); // Third response is mocked to return null and should re-use previous value
-        assertThat(startSessionRequestCaptor.getValue().applicationIdentifier()).isEqualTo(applicationName);
-        assertThat(startSessionRequestCaptor.getValue().environmentIdentifier()).isEqualTo(environmentName);
-        assertThat(startSessionRequestCaptor.getValue().configurationProfileIdentifier()).isEqualTo(defaultTestKey);
+        assertThat(returnedValue4).isEqualTo(secondResponse.configuration()
+                .asUtf8String()); // Forth response is mocked to return empty and should re-use previous value
+        assertThat(startSessionRequestCaptor.getValue().applicationIdentifier()).isEqualTo(APPLICATION_NAME);
+        assertThat(startSessionRequestCaptor.getValue().environmentIdentifier()).isEqualTo(ENVIRONMENT_NAME);
+        assertThat(startSessionRequestCaptor.getValue().configurationProfileIdentifier()).isEqualTo(DEFAULT_TEST_KEY);
         assertThat(getLatestConfigurationRequestCaptor.getAllValues().get(0).configurationToken()).isEqualTo(
                 firstSession.initialConfigurationToken());
         assertThat(getLatestConfigurationRequestCaptor.getAllValues().get(1).configurationToken()).isEqualTo(
@@ -119,8 +127,7 @@ public class AppConfigProviderTest {
     }
 
     @Test
-    public void getValueNoValueExists() {
-
+    void getValueNoValueExists() {
         // Arrange
         StartConfigurationSessionResponse session = StartConfigurationSessionResponse.builder()
                 .initialConfigurationToken("token1")
@@ -134,11 +141,10 @@ public class AppConfigProviderTest {
                 .thenReturn(response);
 
         // Act
-        String returnedValue = provider.getValue(defaultTestKey);
-
+        String returnedValue = provider.getValue(DEFAULT_TEST_KEY);
 
         // Assert
-        assertThat(returnedValue).isEqualTo(null);
+        assertThat(returnedValue).isNull();
     }
 
     /**
@@ -146,7 +152,7 @@ public class AppConfigProviderTest {
      * work as expected. This means two separate configuration sessions should be established with AppConfig.
      */
     @Test
-    public void multipleKeysRetrievalWorks() {
+    void multipleKeysRetrievalWorks() {
         // Arrange
         String param1Key = "key1";
         StartConfigurationSessionResponse param1Session = StartConfigurationSessionResponse.builder()
@@ -184,49 +190,45 @@ public class AppConfigProviderTest {
                 param1Session.initialConfigurationToken());
         assertThat(getLatestConfigurationRequestCaptor.getAllValues().get(1).configurationToken()).isEqualTo(
                 param2Session.initialConfigurationToken());
-
     }
 
     @Test
-    public void getMultipleValuesThrowsException() {
-
+    void getMultipleValuesThrowsException() {
         // Act & Assert
         assertThatRuntimeException().isThrownBy(() -> provider.getMultipleValues("path"))
                 .withMessage("Retrieving multiple parameter values is not supported with the AWS App Config Provider");
     }
 
     @Test
-    public void testAppConfigProviderBuilderMissingEnvironment_throwsException() {
-
+    void testAppConfigProviderBuilderMissingEnvironment_throwsException() {
         // Act & Assert
         assertThatIllegalStateException().isThrownBy(() -> AppConfigProvider.builder()
-                        .withCacheManager(new CacheManager())
-                        .withApplication(applicationName)
-                        .withClient(client)
-                        .build())
+                .withCacheManager(new CacheManager())
+                .withApplication(APPLICATION_NAME)
+                .withClient(client)
+                .build())
                 .withMessage("No environment provided; please provide one");
     }
 
     @Test
-    public void testAppConfigProviderBuilderMissingApplication_throwsException() {
-
+    void testAppConfigProviderBuilderMissingApplication_throwsException() {
         // Act & Assert
         assertThatIllegalStateException().isThrownBy(() -> AppConfigProvider.builder()
-                        .withCacheManager(new CacheManager())
-                        .withEnvironment(environmentName)
-                        .withClient(client)
-                        .build())
+                .withCacheManager(new CacheManager())
+                .withEnvironment(ENVIRONMENT_NAME)
+                .withClient(client)
+                .build())
                 .withMessage("No application provided; please provide one");
     }
-    @Test
-    public void testAppConfigProvider_withoutParameter_shouldHaveDefaultTransformationManager() {
 
+    @Test
+    void testAppConfigProvider_withoutParameter_shouldHaveDefaultTransformationManager() {
         // Act
         AppConfigProvider appConfigProvider = AppConfigProvider.builder()
                 .withEnvironment("test")
                 .withApplication("app")
                 .build();
         // Assert
-        assertDoesNotThrow(()->appConfigProvider.withTransformation(json));
+        assertDoesNotThrow(() -> appConfigProvider.withTransformation(json));
     }
 }
