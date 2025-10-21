dd7a49c25a6193a729a68dcb03f9d3f33af44920
Philipp Page <github@philipp.page>
2025-10-21 11:13:34 +0200

    Fix flaky unit tests. (#2211)

--- powertools-logging/powertools-logging-log4j/src/test/java/org/apache/logging/log4j/layout/template/json/resolver/PowertoolsResolverArgumentsTest.java
+++ powertools-logging/powertools-logging-log4j/src/test/java/org/apache/logging/log4j/layout/template/json/resolver/PowertoolsResolverArgumentsTest.java
@@ -59,9 +59,13 @@ class PowertoolsResolverArgumentsTest {
 
     @AfterEach
     void cleanUp() throws IOException {
-        // Make sure file is cleaned up before running full stack logging regression
-        FileChannel.open(Paths.get("target/logfile.json"), StandardOpenOption.WRITE).truncate(0).close();
-        FileChannel.open(Paths.get("target/ecslogfile.json"), StandardOpenOption.WRITE).truncate(0).close();
+        try {
+            // Make sure file is cleaned up before running full stack logging regression
+            FileChannel.open(Paths.get("target/logfile.json"), StandardOpenOption.WRITE).truncate(0).close();
+            FileChannel.open(Paths.get("target/ecslogfile.json"), StandardOpenOption.WRITE).truncate(0).close();
+        } catch (NoSuchFileException e) {
+            // may not be there in the first run
+        }
     }
 
     @Test
