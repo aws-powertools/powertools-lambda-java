c694ff82116fba68cc51cff79e37552f6f9fc8e0
Cornelius Matejka <cornelius.matejka@cmtjk.de>
2026-02-14 23:01:45 +0100

    feat(logging): add e2e tests for fluent api

---
+++ powertools-e2e-tests/handlers/logging-log4j-fluent-api/src/main/java/software/amazon/lambda/powertools/e2e/Function.java
@@ -0,0 +1,41 @@
+/*
+ * Copyright 2023 Amazon.com, Inc. or its affiliates.
+ * Licensed under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package software.amazon.lambda.powertools.e2e;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.slf4j.spi.LoggingEventBuilder;
+import software.amazon.lambda.powertools.logging.Logging;
+import software.amazon.lambda.powertools.logging.PowertoolsLogging;
+
+public class Function implements RequestHandler<Input, String> {
+    private static final Logger LOG = LoggerFactory.getLogger(Function.class);
+
+    @Logging
+    public String handleRequest(Input input, Context context) {
+
+        LoggingEventBuilder loggingEventBuilder = LOG.atInfo().setMessage(input.getMessage());
+        //noinspection ResultOfMethodCallIgnored
+        input.getKeys().forEach(loggingEventBuilder::addKeyValue);
+        loggingEventBuilder.log();
+
+        // Flush buffer manually since we buffer at INFO level to test log buffering
+        PowertoolsLogging.flushBuffer();
+
+        return "OK";
+    }
+}
